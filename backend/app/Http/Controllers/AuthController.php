576dee04b6db62db01719e1d845274f66e03c55f
melinnp <nengmelinp@gmmail.com>
2026-01-15 12:54:56 +0700

    Final Auth V1

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,27 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login (Request $request) 
+    public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
-        if(!Auth::attempt($credentials)) {
+        if (!Auth::attempt($credentials)) {
             return response()->json([
                 'success' => false,
                 'message' => 'Email atau password salah',
             ], 401);
         }
 
-        $request->session()->regenerate();
-
         return response()->json([
             'success' => true,
             'message' => 'Login berhasil',
-            'users' => auth()->user(),
+            'users' => Auth::user(),
         ]);
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,3 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
-Route::post('/login', [AuthController::class, 'login']);
