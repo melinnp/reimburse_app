fdd8302ad5f38488d9d080bf4f02774849c507a7
melinnp <nengmelinp@gmmail.com>
2026-01-19 22:51:58 +0700

    conflic

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'message' => 'JALAN'
+        ]);
+    }
+
+    public function approve($id)
+    {
+        return response()->json([
+            'message' => 'reimburse $id approve'
+        ]);
+    }
+
+    public function reject($id)
+    {
+        return response()->json([
+            'message' => 'reimburse $id rejected'
+        ]);
+    }
 }
