a45bc12ab67fde01c75d95ce1c3dd4b09536c3e8
melinnp <nengmelinp@gmmail.com>
2026-02-02 10:18:58 +0700

    icon mata di login

--- frontend/public/Script/login.js
+++ frontend/public/Script/login.js
@@ -43,3 +43,13 @@ document.getElementById('loginForm').addEventListener('submit', async function (
     console.error(error);
   }
 });
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+const eyeIcon = document.querySelector('#eyeIcon');
+
+togglePassword.addEventListener('click', function () {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  eyeIcon.classList.toggle('bi-eye');
+  eyeIcon.classList.toggle('bi-eye-slash');
+});
